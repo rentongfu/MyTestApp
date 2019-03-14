bf2431586f4f07c183e547f39a99c9e0b3c99327
Tongfu <861454910@qq.com>
2019-03-14 21:17:53 +0800

    activity滑动退出效果完善

--- app/src/main/java/com/tongfu/mytestapp/smoothexit/SmoothExitActivity.java
+++ app/src/main/java/com/tongfu/mytestapp/smoothexit/SmoothExitActivity.java
@@ -1,5 +1,7 @@
 package com.tongfu.mytestapp.smoothexit;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.drawable.ColorDrawable;
@@ -10,6 +12,8 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.Animation;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
@@ -53,6 +57,12 @@ public class SmoothExitActivity extends AppCompatActivity {
                 return convertView;
             }
         });
+        String[] list  = new String[20];
+        for( int i = 0 ; i < list.length ; i++){
+            list[i] = "列表项：" + i ;
+        }
+
+        listView.setAdapter(new ArrayAdapter<String>(this , android.R.layout.simple_list_item_1 , list ));
     }
 
     Float downX = null ;
@@ -61,10 +71,10 @@ public class SmoothExitActivity extends AppCompatActivity {
     public boolean dispatchTouchEvent(MotionEvent ev) {
         switch(ev.getAction()){
             case MotionEvent.ACTION_DOWN:{
+                Log.i("SmoothExit" , "当前的横坐标为：" + ev.getX() ) ;
 //                downPoint = new Point((int)ev.getX() , (int)ev.getY());
-                if(ev.getX() < 10){
+                if(ev.getX() < 100){
                     downX = ev.getX();
-                    Log.i("SmoothExit" , "当前的横坐标为：" + downX ) ;
                     return true ;
                 }
                 break;
@@ -81,15 +91,52 @@ public class SmoothExitActivity extends AppCompatActivity {
             }
             case MotionEvent.ACTION_UP:{
                 if(downX !=null){
-                    getWindow().getDecorView().setTranslationX(0);
+                    int screenWidth = getResources().getDisplayMetrics().widthPixels;
+                    if(ev.getX() / screenWidth > 0.3){
+                        goOnToExit(downX , ev.getX());
+                    }else{
+                        goToReset(downX , ev.getX());
+                    }
                     downX = null ;
                     return true;
                 }
                 break;
-
             }
         }
-
         return super.dispatchTouchEvent(ev);
+    }
+
+    private void goToReset(float downX , float currentX) {
+        int screenWidth = getResources().getDisplayMetrics().widthPixels;
+        View view = getWindow().getDecorView();
+        float currentDeltaX = currentX-downX ;
+        Animator a = ObjectAnimator.ofFloat(view , "x" , currentDeltaX , 0 );
+        a.setDuration((long) (300 * currentDeltaX / screenWidth));
+        a.start();
+    }
+
+    private void goOnToExit(float downX , float currentX) {
+        int screenWidth = getResources().getDisplayMetrics().widthPixels;
+        View view = getWindow().getDecorView();
+        float currentDeltaX = currentX-downX ;
+        Animator a = ObjectAnimator.ofFloat(view , "x" , currentDeltaX , screenWidth );
+        a.setDuration((long) (300 * currentDeltaX / screenWidth));
+        a.start();
+        a.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) { }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                finish();
+                overridePendingTransition(0,0);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) { }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) { }
+        });
     }
 }
