11d02f24b293e4b55cde20b802722447968243eb
tongfu <861454910@qq.com>
2021-03-03 10:00:26 +0800

    surfaceView demo

--- app/src/main/java/com/tongfu/mytestapp/uiwidget/surfaceview/SurfaceViewActivity.java
+++ app/src/main/java/com/tongfu/mytestapp/uiwidget/surfaceview/SurfaceViewActivity.java
@@ -20,32 +20,68 @@ public class SurfaceViewActivity extends AppCompatActivity {
     @BindView(R.id.surfaceView)
     SurfaceView surfaceView ;
 
+    private Thread refreshThread = null ;
+    private long lastRefreshTime = 0 ;
+    private boolean isGoingOn = true ;
+
+    private int surfaceWidth = 0;
+    private int surfaceHeight = 0 ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_surface_view);
         ButterKnife.bind(this);
-
+        surfaceWidth = surfaceView.getWidth();
+        surfaceHeight = surfaceView.getHeight() ;
         surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                Canvas canvas = surfaceView.getHolder().lockCanvas();
-                canvas.drawColor(Color.BLUE);
-                Paint paint = new Paint();
-                paint.setColor(Color.BLACK);
-                canvas.drawRect(new Rect( 0 , 0 , 100 , 100) , paint);
+                if(refreshThread==null){
+                    refreshThread = new Thread(){
+                        private int currentX = 0 , currentY = 0 ;
+                        private boolean rightMove = true , downMove = true ;
+                        @Override
+                        public void run() {
+                            while(isGoingOn ){
+                                if(System.currentTimeMillis() - lastRefreshTime<16){
+                                    continue;
+                                }
+                                lastRefreshTime = System.currentTimeMillis();
+                                if(currentX == 0)rightMove = true;
+                                else if(currentX+100 == surfaceWidth) rightMove = false ;
+                                if(currentY == 0) downMove = true ;
+                                else if(currentY +100 == surfaceHeight) downMove = false;
+                                if(rightMove) currentX++;
+                                else currentX--;
+                                if(downMove) currentY++;
+                                else currentY--;
+
+                                Canvas canvas = surfaceView.getHolder().lockCanvas();
+                                canvas.drawColor(Color.BLUE);
+                                Paint paint = new Paint();
+                                paint.setColor(Color.BLACK);
+                                canvas.drawRect(new Rect( currentX , currentY , currentX + 100  , currentY + 100) , paint);
 //        surfaceView.draw(canvas);
-                surfaceView.getHolder().unlockCanvasAndPost(canvas);
+                                surfaceView.getHolder().unlockCanvasAndPost(canvas);
+                            }
+                        }
+                    };
+                }
+                refreshThread.start();
+
             }
 
             @Override
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
+                surfaceHeight = height;
+                surfaceWidth = width;
             }
 
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
-
+                isGoingOn = false;
+                refreshThread = null ;
             }
         });
 
