652be5bd92d9d5a412686e86acafcb8b72796300
tongfu <861454910@qq.com>
2021-03-03 10:00:45 +0800

    自定义View demo完善

--- app/src/main/java/com/tongfu/mytestapp/customview/ontouchevent1/MyView.java
+++ app/src/main/java/com/tongfu/mytestapp/customview/ontouchevent1/MyView.java
@@ -3,13 +3,17 @@ package com.tongfu.mytestapp.customview.ontouchevent1;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * 自定义View，每次绘制将绘制成随机颜色。
+ *
+ *
  */
 public class MyView extends View {
     public MyView(Context context) {
@@ -31,17 +35,46 @@ public class MyView extends View {
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        Log.i("MyView" , "onLayout");
         super.onLayout(changed, left, top, right, bottom);
     }
 
     @Override
     public void layout(int l, int t, int r, int b) {
+        Log.i("MyView" , "layout");
         super.layout(l, t, r, b);
     }
 
+    private final int INIT_WIDTH = 200 ;
+    private final int INIT_HEIGHT = 100 ;
+
+    /**
+     * 页面打开时调用，内部必须调用setMeasuredDimension。传入的值后续会影响layout和onLayout方法接受的值。
+     * 理论上也可以重写layout方法，将计算宽高的逻辑放在layout方法里面。本demo中onTouchEvent中就是这么做的。
+     */
+    private int mWidth = INIT_WIDTH , mHeight = INIT_HEIGHT ;
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Log.i("MyView" , "onMeasure");
+//        System.out.println("width:" + widthMeasureSpec + "height:" + heightMeasureSpec);
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(INIT_WIDTH , INIT_HEIGHT);
+    }
+
+
+    private final int[] colors = {Color.RED , Color.GREEN , Color.BLACK , Color.GREEN , Color.BLUE , Color.DKGRAY , Color.LTGRAY , Color.YELLOW , Color.MAGENTA};
+
+    /**
+     * 触发时机：
+     *  调用invalidate()之后
+     *  layout方法的入参计算出的宽度和高度发生改变时自动调用。
+     * @param canvas
+     */
     @Override
     protected void onDraw(Canvas canvas) {
+        Log.i("MyView" , "onDraw");
         super.onDraw(canvas);
+        canvas.drawColor(colors[(int)(Math.random()* colors.length)]);
     }
 
     int lastX,lastY ;
@@ -50,6 +83,7 @@ public class MyView extends View {
     public boolean onTouchEvent(MotionEvent event) {
         int x = (int)event.getX() ;
         int y = (int)event.getY() ;
+        Log.i("MyView" , "onTouchEvent x:" + x +" y:" + y);
 
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:{
@@ -60,7 +94,16 @@ public class MyView extends View {
             case MotionEvent.ACTION_MOVE:{
                 int offsetX = x - lastX ;
                 int offsetY = y - lastY ;
-                layout(getLeft() + offsetX , getTop() + offsetY , getRight() + offsetX , getBottom() + offsetY );
+                if(x < 50 || y <50){
+                    mHeight = mHeight + offsetY ;
+                    mWidth = mWidth + offsetX ;
+                }
+                layout(getLeft() + offsetX , getTop() + offsetY , getLeft() + offsetX + mWidth , getTop() + offsetY + mHeight );
+
+                break;
+            }
+            case MotionEvent.ACTION_UP:{
+                invalidate();
                 break;
             }
             default:
@@ -72,6 +115,7 @@ public class MyView extends View {
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
+        Log.i("MyView" , "dispatchTouchEvent");
         return super.dispatchTouchEvent(event);
     }
 }
--- app/src/main/java/com/tongfu/mytestapp/customview/ontouchevent1/MyViewGroup.java
+++ app/src/main/java/com/tongfu/mytestapp/customview/ontouchevent1/MyViewGroup.java
@@ -3,13 +3,16 @@ package com.tongfu.mytestapp.customview.ontouchevent1;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 /**
  * 自定义ViewGroup
  * 其子View将会显示在随机位置。
  */
-public class MyViewGroup extends ViewGroup {
+public class MyViewGroup extends FrameLayout {
     public MyViewGroup(Context context) {
         super(context);
     }
@@ -27,17 +30,38 @@ public class MyViewGroup extends ViewGroup {
     }
 
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.i("MyViewGroup" , "onTouchEvent");
+        return super.onTouchEvent(event);
+    }
 
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        Log.i("MyViewGroup" , "onInterceptTouchEvent");
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        Log.i("MyViewGroup" , "dispatchTouchEvent");
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        Log.i("MyViewGroup" , "onLayout");
+        super.onLayout(changed, l , t , r , b);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Log.i("MyViewGroup" , "onMeasure");
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        Log.i("MyViewGroup" , "onDraw");
         super.onDraw(canvas);
     }
 
