6bae27d281732ece800872b40891ae57a6334461
tongfu <861454910@qq.com>
2021-03-03 17:28:58 +0800

    glide load image demo

--- app/src/main/java/com/tongfu/mytestapp/imageload/GlideActivity.kt
+++ app/src/main/java/com/tongfu/mytestapp/imageload/GlideActivity.kt
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity
 import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.squareup.picasso.Picasso
 import com.tongfu.mytestapp.R
 
@@ -33,7 +35,8 @@ class GlideActivity : AppCompatActivity() {
         }
 
 //        Glide.with()
+        Glide.with(this).load(url).apply(RequestOptions.fitCenterTransform()).into(iv_image)
 
-        Picasso.with(this).load(url).placeholder(R.drawable.ic_blank).error(R.drawable.ic_error).into(iv_image)
+//        Picasso.with(this).load(url).placeholder(R.drawable.ic_blank).error(R.drawable.ic_error).into(iv_image)
     }
 }
